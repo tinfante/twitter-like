531151bd1f34098d683f5c76b2329b097a90b812
tinfante <correo@verdadero.cl>
2017-09-27 21:43:58 -0300

    auth credentials now go in auth.data file, not commited here. also added a sample for that file as auth.data.sample.

--- cogni-like.py
+++ cogni-like.py
@@ -1,20 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+from time import sleep
+import sys
 import tweepy
 
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+
+target = 'cognitiva_la'
+num_tweets = 10
+include_rts = False
+wait = 0.1
+
+
+if not os.path.isfile('auth.data'):
+    sys.exit('auth.data file not found!')
+with open('auth.data', 'r') as f:
+    lines = [l.strip() for l in f.readlines() if l.strip()]
+auth_data = dict(l.split('=', 1) for l in lines)
+req_keys = [
+        'consumer_key', 'consumer_secret',
+        'access_token', 'access_token_secret'
+        ]
+if not all(rk in auth_data.keys() for rk in req_keys):
+    sys.exit(
+            "auth.data necesita tener las variables '"
+            "'consumer_key', 'consumer_secret', "
+            "'access_token', 'access_token_secret'."
+            )
+
+consumer_key = auth_data['consumer_key']
+consumer_secret = auth_data['consumer_secret']
+access_token = auth_data['access_token']
+access_token_secret = auth_data['access_token_secret']
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+tweets = api.user_timeline(
+        screen_name=target,
+        count=num_tweets,
+        include_rts=include_rts
+        )
 
-tweets = api.user_timeline(screen_name = 'cognitiva_la', count = 10, include_rts = True)
 for tweet in tweets:
+    print(tweet)
+    print()
+    print(dir(tweet))
+    print()
+    print('text: ' + tweet.text)
+    print()
     if not tweet.favorited:
-        tweet.favorite()
+        #tweet.favorite()
+        pass
+    sleep(wait)
